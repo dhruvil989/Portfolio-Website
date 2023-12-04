1fbd841e72646615eed3bb9e2b64815cc360627d
darshan <darshanmoradiya989@gmail.com>
2023-12-04 11:24:22 +0530

    - Added AOS Animation - Made Website Responsive

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#project-1").click(function(){
+        $("#more1").toggleClass("d-none");
+        $("#line-clamp-1").toggleClass("line-clamp-2");
+        $('#less1').toggleClass('d-block');
+    });
+    $("#project-2").click(function(){
+        $("#more2").toggleClass("d-none");
+        $("#line-clamp-2").toggleClass("line-clamp-2");
+        $('#less2').toggleClass('d-block');
+    }); 
+    $("#project-3").click(function(){
+        $("#more3").toggleClass("d-none");
+        $("#line-clamp-3").toggleClass("line-clamp-2");
+        $('#less3').toggleClass('d-block');
+    });
+    $("#project-4").click(function(){
+        $("#more4").toggleClass("d-none");
+        $("#line-clamp-4").toggleClass("line-clamp-2");
+        $('#less4').toggleClass('d-block');
+    });
+})
